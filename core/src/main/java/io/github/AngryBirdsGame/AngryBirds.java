23c11d96eb6a593b4b5389acd0e135c586ff4984
simar23527 <162867408+simar23527@users.noreply.github.com>
2024-11-25 14:08:57 +0530

    Updated AngryBirds.java

--- core/src/main/java/io/github/AngryBirdsGame/AngryBirds.java
+++ core/src/main/java/io/github/AngryBirdsGame/AngryBirds.java
@@ -9,10 +9,18 @@ import io.github.AngryBirdsGame.Pages.LoadingPage;
 
 
 public class AngryBirds extends Game{
-
     public SpriteBatch batch;
     public Stage stage;
     private Box2DWorld box2DWorld;
+    private int currentLevel;
+
+    public int getCurrentLevel(){
+        return this.currentLevel;
+    }
+
+    public void setCurrentLevel(int l){
+        this.currentLevel=l;
+    }
 
     @Override
     public void create() {
@@ -20,17 +28,22 @@ public class AngryBirds extends Game{
         stage = new Stage();
         box2DWorld=new Box2DWorld();
         this.setScreen(new LoadingPage(this));
+
     }
 
     @Override
     public void render() {
         super.render();
+        // Update and render the Box2D world
         box2DWorld.update(Gdx.graphics.getDeltaTime());
         box2DWorld.render();
 
+        // Render other game objects and stage elements (UI)
         batch.begin();
+        // Render your game objects (e.g., birds, blocks)
         batch.end();
 
+        // Draw the UI elements
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
         stage.draw();
     }
