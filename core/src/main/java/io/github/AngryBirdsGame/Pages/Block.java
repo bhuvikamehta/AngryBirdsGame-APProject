f99e8ced1c9eb9b45414540f205cafffbc4b6cd1
simar23527 <162867408+simar23527@users.noreply.github.com>
2024-11-30 22:19:10 +0530

    Updated Block as abstract class.java

--- core/src/main/java/io/github/AngryBirdsGame/Pages/Block.java
+++ core/src/main/java/io/github/AngryBirdsGame/Pages/Block.java
@@ -7,8 +7,34 @@ import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import io.github.AngryBirdsGame.AngryBirds;
 
-public class Block extends GameObject {
+public abstract class Block extends GameObject implements PhysicsBody{
+    private float health;
+    private Level level;
+    private static final float BLOCK_DAMAGE_MULTIPLIER = 0.5f;
+    private float durability;
+    private AngryBirds game;
+    private float destroyTimer=-1;
+    private boolean breaking=false;
+
+    protected abstract float getDensity();
+    protected abstract float getFriction();
+    protected abstract float getRestitution();
+
+    public void setBreaking(boolean breaking) {
+        this.breaking = breaking;
+    }
+
+    public boolean isBreaking() {
+        return breaking;
+    }
+
+    public void setDestroyTimer(float seconds) {
+        this.destroyTimer = seconds;
+    }
+
+
     @Override
     public void render() {
 
@@ -16,17 +42,98 @@ public class Block extends GameObject {
 
     private String type;
     private Body blockie;
-    private float health = 0.2f;  // Initial health
-    private boolean isDestroying = false;
+    private boolean destroyed;
 
-    public Block(World world, String texturePath, String type, float scale, float posX, float posY) {
+    public Block(World world, String texturePath, String type, float scale, float posX, float posY, AngryBirds g) {
         super(world, texturePath, scale, posX, posY);
         this.type=type;
+        this.destroyed=false;
         objectSprite.setScale(scale);
         objectSprite.setPosition(posX, posY);
         createBody(BodyDef.BodyType.DynamicBody);
-       // body.setUserData(this);
+        this.durability=20;
+        this.game=g;
+        blockie.setUserData(this);
+        initBlockProperties();
 
+    }
+    protected abstract void initBlockProperties();
+
+    public boolean isDestroyed(){
+        return destroyed;
+    }
+
+    public void setDestroyed(boolean v){
+        this.destroyed=v;
+    }
+
+    public void setLevel(Level level) {
+        this.level = level;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Body getBlockie() {
+        return blockie;
+    }
+
+    public void setBlockie(Body blockie) {
+        this.blockie = blockie;
+    }
+
+    public void reduceDurability(float damage) {
+        this.durability -= damage;
+        if (this.durability <= 0) {
+            setDestroyed(true);
+        }
+    }
+
+    public float getDurability(){
+        return this.durability;
+    }
+
+    private void markForDestruction() {
+        if (game != null && game.getCurrentLevel() != 0) {
+            if(game.getCurrentLevel()==1){
+                game.getLeve1().markForDestruction(this);
+            }
+            else if(game.getCurrentLevel()==2){
+                game.getLeve2().markForDestruction(this);
+            }
+            else if(game.getCurrentLevel()==3){
+                game.getLeve3().markForDestruction(this);
+            }
+
+
+        }
+    }
+
+    public float getHealth() {
+        return health;
+    }
+
+    public void setHealth(float health) {
+        this.health = health;
+    }
+
+
+    public boolean isAlive(){
+        if(this.health<=0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    public void setAlive(boolean b){
+        this.isAlive=b;
     }
 
     @Override
@@ -43,10 +150,9 @@ public class Block extends GameObject {
         );
         FixtureDef fixtureDef = new FixtureDef();
         fixtureDef.shape = shape;
-        fixtureDef.density = 0.0f;  // Static bodies don't need mass
-        fixtureDef.friction = 0.5f;
-        fixtureDef.restitution = 0.0f;  // No bounce for blocks
-
+        fixtureDef.density = 5.0f;  // Static bodies don't need mass
+        fixtureDef.friction = 0.4f;
+        fixtureDef.restitution = 0.2f;// No bounce for blocks
         blockie.createFixture(fixtureDef);
         shape.dispose();
 
@@ -69,12 +175,9 @@ public class Block extends GameObject {
         shape.dispose();
     }
 
-//    @Override
-//    public void render(SpriteBatch batch) {
-//
-//    }
 
     public void draw(SpriteBatch batch) {
+
         objectSprite.draw(batch);
     }
 
@@ -86,19 +189,7 @@ public class Block extends GameObject {
         objectSprite.setPosition(x, y);
     }
 
-    public Object getUserData() {
-        if (blockie != null) {
-            return blockie.getUserData();
-        }
-        return null;
-    }
 
-
-
-
-    public String getType() {
-        return type;
-    }
 
 
     @Override
@@ -116,24 +207,46 @@ public class Block extends GameObject {
         objectSprite.getTexture().dispose();
     }
 
-    public void takeDamage(float damage) {
+    public void takeDamage(float impactForce) {
+        float damage = impactForce * BLOCK_DAMAGE_MULTIPLIER;
         health -= damage;
         if (health <= 0) {
-            isDestroying = true;
+            destroyed = true;
         }
     }
 
-    public void update(float deltaTime) {
-        if (isDestroying) {
-            // Gradually reduce scale and fade out
-            float reductionRate = 0.5f;  // Adjust for desired destruction speed
-            sizeObject -= reductionRate * deltaTime;
-            objectSprite.setScale(sizeObject);
+    @Override
+    public void update() {
+        if (isDestroyed()) {
+            // Destroy the block's body and remove it from the world
+            if (blockie != null) {
+                //blockie.setType(BodyDef.BodyType.DynamicBody);
+                world.destroyBody(blockie);
+                blockie = null; // Prevent further interaction
+            }
+        } else {
 
-            // Remove from physics world when completely destroyed
-            if (sizeObject <= 0) {
+            // Synchronize sprite position with the physics body
+            if (blockie != null) {
+                Vector2 position = blockie.getPosition();
+                objectSprite.setPosition(
+                    (position.x * PIXELS_TO_METERS) - (objectSprite.getWidth() / 2),
+                    (position.y * PIXELS_TO_METERS) - (objectSprite.getHeight() / 2)
+                );
+                objectSprite.setRotation((float) Math.toDegrees(blockie.getAngle()));
+            }
+        }
+    }
+
+    @Override
+    public void destroyBody() {
+        if (blockie != null && world != null) {
+            try {
                 world.destroyBody(blockie);
                 blockie = null;
+            } catch (Exception e) {
+                System.err.println("Error destroying block body: " + e.getMessage());
+                e.printStackTrace();
             }
         }
     }
@@ -141,6 +254,4 @@ public class Block extends GameObject {
     public void handleCollision(float impactForce) {
         takeDamage(impactForce);
     }
-
-
 }
